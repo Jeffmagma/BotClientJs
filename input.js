e7a677f9560e4f58e28426a6c819dcaf8626bfcb
Jeffmagma <jeffrey.magmachu@gmail.com>
2018-11-27 17:44:36 -0500

    made some changes, some stuff works now

--- input.js
+++ input.js
@@ -9,6 +9,7 @@ const channel_div = $('#channels');
 const message_div = $('#messages');
 
 let currentGuild, currentChannel;
+let guild_buttons, channel_buttons;
 
 input.keyup(event => {
 	if (event.keyCode === 13) {
@@ -17,28 +18,40 @@ input.keyup(event => {
 	}
 });
 
+function updateMessages() {
+	if (currentChannel.type === "text") console.log(currentChannel.fetchMessages({limit: 100})); //message_div.append(currentChannel.fetchMessages({limit: 100}));
+}
+
 function updateGuilds() {
 	guild_div.val('');
 	guild_div.append('guilds:');
-	let guild_buttons = [], channel_buttons = [];
+	/*let*/
+	guild_buttons = [];
+	channel_buttons = [];
 	for (const [g_id, g] of client.guilds) {
-		guild_buttons.push(guild_div.append('<button id="' + g.id + '">' + g.name + '</button>'));
-		guild_buttons[guild_buttons.length - 1].click(() => {
+		let current_button = $('<button id="' + g_id + '">' + g.name + '</button>');
+		current_button.click(() => {
 			currentGuild = g;
-			channel_div.val('');
+			channel_div.empty();
 			channel_div.append('channels:');
-			for (const [c_id, c] of g.channels) {
-				channel_buttons.push(channel_div.append('<button id="' + c.id + '">' + c.name + '</button>'));
-				channel_buttons[channel_buttons.length - 1].click(() => {
+			for (const [c_id, c] of currentGuild.channels) {
+				let current_channel_button = $('<button id="' + c_id + '">' + c.name + '</button>');
+				current_channel_button.click(() => {
 					currentChannel = c;
+					updateMessages()
 				});
+				channel_div.append('<br>').append(current_channel_button);
+				channel_buttons.push(current_channel_button);
 			}
 		});
+		guild_div.append('<br>').append(current_button);
+		guild_buttons.push(current_button);
 	}
 }
 
 client.on('message', msg => {
-	message_div.append('<br>' + msg.content);
+	//console.log(`${msg.guild.name}#${msg.channel.name} ${msg.author.name}: ${msg.content}\n`);
+	message_div.append(`<br>${msg.guild.name}#${msg.channel.name} ${msg.author.name}: ${msg.content}`);
 });
 
 client.on('guildCreate', g => {
@@ -50,7 +63,7 @@ client.on('guildDelete', g => {
 });
 
 client.on('ready', () => {
-	client.user.setActivity("EDEN", {type: "LISTENING"});
+	//client.user.setActivity("myself being hacked", {type: "WATCHING"});
 	updateGuilds();
 });
 
